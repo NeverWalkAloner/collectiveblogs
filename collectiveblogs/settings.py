98c9ed77397ef0a6772e63b21afefa84a50cf719
NeverWalkAloner <NeverWalkAloner@gmail.com>
2016-11-23 15:38:28 +0500

    Avatars added to user model

--- collectiveblogs/settings.py
+++ collectiveblogs/settings.py
@@ -132,3 +132,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap3'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- users/models.py
+++ users/models.py
@@ -3,11 +3,19 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+def upload_location(instance, filename):
+    return 'avatars/{}/{}'.format(instance.id, filename)
+
+
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     about = models.TextField('О себе', blank=True)
     karma = models.PositiveIntegerField(verbose_name='карма', default=0)
+    avatar = models.ImageField(verbose_name='Аватар',
+                               upload_to=upload_location,
+                               null=True,
+                               blank=True)
 
     def __str__(self):
         return self.user.username
