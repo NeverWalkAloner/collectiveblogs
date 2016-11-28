6c4f774f6ce7dcf84e828ccfd60978c455cd88a7
NeverWalkAloner <NeverWalkAloner@gmail.com>
2016-11-28 20:33:09 +0500

    blogs models added

--- blogs/models.py
+++ blogs/models.py
@@ -1,8 +1,22 @@
 # -*- coding: utf-8 -*-
 from django.db import models
+from django.core.validators import RegexValidator
+from django.contrib.auth.models import User
 
 
-# Create your models here.
+unique_validator = RegexValidator(r'[\w-]+', 'Допустимы только символы латинского алфавита и знак -')
+
+
 class Blog(models.Model):
     name = models.CharField('Наименование', max_length=50)
     description = models.TextField('Описание', max_length=255)
+    unique_name = models.CharField('Уникальные идентификатор',
+                                   max_length=50,
+                                   unique=True,
+                                   blank=False,
+                                   validators=[unique_validator])
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
