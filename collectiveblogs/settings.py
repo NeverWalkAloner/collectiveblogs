50e7ae3119c3964b2fce701fc4eb6daacd99a99e
NeverWalkAloner <NeverWalkAloner@gmail.com>
2016-12-25 13:06:00 +0500

    Debug settings fixed

--- collectiveblogs/settings.py
+++ collectiveblogs/settings.py
@@ -27,7 +27,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+if os.environ.get('DEBUG') == 'True':
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = []
 
